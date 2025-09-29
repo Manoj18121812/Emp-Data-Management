52614c6e9ebc8778f30a34dfd95dd8be69ea709b
Manoj18121812 <gavhaneam@gmail.com>
2025-09-29 20:21:42 +0530

    Project Start

---
+++ src/main/java/com/example/Emp_Data_Management/EmpDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.Emp_Data_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDataManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Emp_Data_Management/EmpDataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Emp_Data_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpDataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
