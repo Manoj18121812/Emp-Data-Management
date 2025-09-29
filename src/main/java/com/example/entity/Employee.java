98016d05a1b8fa7707e6ebd58cd31c82d1fbd48f
Manoj18121812 <gavhaneam@gmail.com>
2025-09-29 21:06:21 +0530

    employee entity service

--- src/main/java/com/example/entity/Employee.java
+++ src/main/java/com/example/entity/Employee.java
@@ -47,4 +47,13 @@ public class Employee {
     }
 
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
 }
