8159e29c5cbaf78bbea706405b3207ca7fe62e98
Manoj18121812 <gavhaneam@gmail.com>
2025-09-29 21:04:25 +0530

    employee entity service

---
+++ src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,50 @@
+package com.example.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+
+    private String name;
+    private String email;
+    private String position;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
+}
