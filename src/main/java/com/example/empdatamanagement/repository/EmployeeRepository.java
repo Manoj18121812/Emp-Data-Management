ad6384313390c8c48ebc3f6c962e93e129daee89
Manoj18121812 <gavhaneam@gmail.com>
2025-10-03 09:12:18 +0530

    EmployyeRepo modify

--- src/main/java/com/example/empdatamanagement/repository/EmployeeRepository.java
+++ src/main/java/com/example/empdatamanagement/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
 package com.example.empdatamanagement.repository;
 
-import com.example.empdatamanagement.entity.Employee;   // ✅ not model
+import com.example.empdatamanagement.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
